7623dac43a3ba8a3825778b184d9b5aad1ca5ac0
AvaOleh <avakimyanov@gmail.com>
2023-01-17 18:04:52 +0300

    add helpers for searching elements

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,9 @@
-class BasePage():
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
@@ -6,3 +11,24 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    def element_is_visible(self, locator, timeout=5):
+        return wait(self.browser, timeout).until(EC.visibility_of_element_located(locator))
+
+    def elements_are_visible(self, locator, timeout=5):
+        return wait(self.browser, timeout).until(EC.visibility_of_all_elements_located(locator))
+
+    def element_is_present(self, locator, timeout=5):
+        return wait(self.browser, timeout).until(EC.presence_of_element_located(locator))
+
+    def elements_are_present(self, locator, timeout=5):
+        return wait(self.browser, timeout).until(EC.presence_of_all_elements_located(locator))
+
+    def element_is_not_visible(self, locator, timeout=5):
+        return wait(self.browser, timeout).until(EC.invisibility_of_element_located(locator))
+
+    def element_is_clickable(self, locator, timeout=5):
+        return wait(self.browser, timeout).until(EC.element_to_be_clickable(locator))
+
+    def go_to_element(self, element):
+        self.browser.execute_script("arguments[0].scrollIntoView();", element)

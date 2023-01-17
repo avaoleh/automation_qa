3546b6d31b49aba30fa3aa284fa5f3dc3a477c37
AvaOleh <avakimyanov@gmail.com>
2023-01-17 21:42:06 +0300

    update test elements add test WebTablePage

--- generator/generator.py
+++ generator/generator.py
@@ -4,13 +4,17 @@ from data.data import Person
 from faker import Faker
 
 faker_ru = Faker('ru_RU')
-fake_en = Faker('En')
 Faker.seed()
 
 
 def generated_person():
     yield Person(
         full_name=faker_ru.first_name() + " " + faker_ru.last_name() + " " + faker_ru.middle_name(),
+        firstname=faker_ru.first_name(),
+        lastname=faker_ru.last_name(),
+        age=random.randint(10, 80),
+        salary=random.randint(10000, 100000),
+        department=faker_ru.job(),
         email=faker_ru.email(),
         current_address=faker_ru.address(),
         permanent_address=faker_ru.address()

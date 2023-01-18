d190620f0b59fc4b7d0f147ca2bd043f0d647f2e
AvaOleh <avakimyanov@gmail.com>
2023-01-18 16:51:11 +0300

    update test elements added class:  TestDynamicPropertiesPage added tests:   test_dynamic_properties   test_appear_button   test_enable_button added helper in generator.py:    generated_color

--- data/data.py
+++ data/data.py
@@ -12,3 +12,15 @@ class Person:
     email: str = None
     current_address: str = None
     permanent_address: str = None
+
+@dataclass
+class Color:
+    color_name: list = None
+
+
+@dataclass
+class Date:
+    day: str = None
+    month: str = None
+    year: str = None
+    time: str = None
